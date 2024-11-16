3452e0824c7158744269111e14be7f809d9debd7
tieubochet <tao.nguyen712@gmail.com>
2024-11-16 08:51:20 +0700

    change url

--- app/utils.ts
+++ app/utils.ts
@@ -27,7 +27,7 @@ export function appURL() {
 
 export function vercelURL() {
   return process.env.VERCEL_URL
-    ? `https://hunt-stats-v92-92.vercel.app/`
+    ? `https://hunt-stats-v92.vercel.app/`
     : undefined;
 }
 
