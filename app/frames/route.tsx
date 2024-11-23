8724aa105aefe72d05763a76744e6b429de40078
tieubochet <tao.nguyen712@gmail.com>
2024-11-23 11:05:49 +0700

    change text color

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -120,9 +120,9 @@ const frameHandler = frames(async (ctx) => {
               tw="w-32 h-32 rounded-md mb-2 border border-[#fff]"
             /></div>
           <div tw="flex text-[#fff]">@{huntstatsJSON.username}</div>
-          <div tw="flex mb-3 mt-5">Allowance: <span tw="flex text-[#FFF59D] ml-5 mr-5">{formatNumber(huntstatsJSON.tip_allowance)}</span></div>
-          <div tw="flex mb-3">Daily Remaining: <span tw="flex text-[#FFF59D] ml-5 mr-5">{formatNumber(huntstatsJSON.remaining_allowance)}</span></div>
-          <div tw="flex mb-15">Total Received: <span tw="flex text-[#FFF59D] ml-5 mr-5">{formatNumber(huntstatsJSON.received)}</span></div>
+          <div tw="flex mb-3 mt-5">Allowance: <span tw="flex text-[#ffdc00] ml-5 mr-5">{formatNumber(huntstatsJSON.tip_allowance)}</span></div>
+          <div tw="flex mb-3">Daily Remaining: <span tw="flex text-[#ffdc00] ml-5 mr-5">{formatNumber(huntstatsJSON.remaining_allowance)}</span></div>
+          <div tw="flex mb-15">Total Received: <span tw="flex text-[#ffdc00] ml-5 mr-5">{formatNumber(huntstatsJSON.received)}</span></div>
           
       </div>
     );
