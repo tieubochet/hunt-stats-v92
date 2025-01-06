8c0cc57ebdedb0e6373183bb68b8e4f0953a5709
tieubochet <tao.nguyen712@gmail.com>
2025-01-06 08:29:19 +0700

    add current season

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -140,6 +140,7 @@ const frameHandler = frames(async (ctx) => {
             tw="w-25 h-25 rounded-3 absolute top-85 left-60"
           />
           <div tw="flex mb-0 absolute top-4 right-5">{dateNow}</div>
+          <div tw="flex mb-0 absolute top-4 left-63">#Season 7</div>
           <div tw="flex text-[35px] absolute top-87 left-95 text-black">{userData?.username}</div>
           <div tw="flex text-[26px] absolute top-100 left-95 text-black">FID: {userData?.fid}</div>
           <div tw="flex text-[32px] justify-end absolute top-88 right-55 text-black">Score: {formatNumber(huntstatsJSON.farscore)}</div>
@@ -148,7 +149,7 @@ const frameHandler = frames(async (ctx) => {
           <div tw="flex text-[44px] justify-end absolute top-163 right-66 text-[#FF0F15]">{formatNumber(huntstatsJSON.tipped)} 👏</div>
           <div tw="flex text-[44px] justify-end absolute top-183 right-66 text-[#FF0F15]">{formatNumber(huntstatsJSON.miniBuildingCount)} 👏</div>
           <div tw="flex text-[44px] justify-end absolute top-205 right-66 text-[#f00707]">{formatNumber(huntstatsJSON.received)} 👏</div>
-          <div tw="flex mb-0 absolute bottom-5 left-5 text-[26px] text-[#000000]">Frame Created by @tieubochet.eth</div>
+          <div tw="flex mb-0 absolute bottom-5 left-5 text-[26px] text-[#ffffff]">@tieubochet.eth</div>
       </div>
     );
   };
