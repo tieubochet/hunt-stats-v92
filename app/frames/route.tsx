5b70ef73f21448ddb18ad01c4377d09c4dd0651a
tieubochet <tao.nguyen712@gmail.com>
2025-05-25 12:48:37 +0700

    remove cast action

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -208,13 +208,7 @@ const frameHandler = frames(async (ctx) => {
     buttons.push(
       <Button action="post" target={{ href: `${appURL()}?userfid=${fid}` }}>
         Mine🔎
-      </Button>,
-      <Button
-      action="link"
-        target="https://warpcast.com/~/add-cast-action?url=https%3A%2F%2Fcheck-hunt-stats-v93.vercel.app%2Fapi%2Fcast-action"
-      >
-        Cast Action
-      </Button>,
+      </Button>
     );
   } else {
     buttons.push(
