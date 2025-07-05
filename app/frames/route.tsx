4981ea7976d35307c14a62ca7f71b8de11f80870
tieubochet <tao.nguyen712@gmail.com>
2025-07-05 08:39:52 +0700

    remove cast action

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -212,11 +212,6 @@ const frameHandler = frames(async (ctx) => {
       <Button action="link" target={shareUrl}>
         Share
       </Button>,
-      <Button action="link"
-        target="https://warpcast.com/~/add-cast-action?url=https%3A%2F%2Fcheck-hunt-stats-v93.vercel.app%2Fapi%2Fcast-action"
-      >
-        Cast Action
-      </Button>,
     );
   } else {
     buttons.push(
@@ -225,11 +220,6 @@ const frameHandler = frames(async (ctx) => {
       </Button>,
       <Button action="link" target={shareUrl}>
         Share
-      </Button>,
-      <Button action="link"
-        target="https://warpcast.com/~/add-cast-action?url=https%3A%2F%2Fcheck-hunt-stats-v93.vercel.app%2Fapi%2Fcast-action"
-      >
-        Cast Action
       </Button>,
       
     );
