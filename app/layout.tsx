5af3d865d5aef0fb7b95600e7d4e416456a533c0
tieubochet <tao.nguyen712@gmail.com>
2026-03-28 08:29:50 +0700

    add verify

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ const latin = Baloo_2({ weight: ["400"], subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hunt Stats Frame v1",
   description: "Check your HUNT STATS by @tieubochet.eth",
+  other: {
+    "name": "talentapp:project_verification",
+    "content": "b70c8833bbf0f77768198ee82925ee16a05a8b87eb5a34dd3d19d129f7f73c7327123836617fefcd7c04d96933d76707431f8f0651e20ba62a8aee8b9cd64d9a"
+  }
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,9 @@ export async function generateMetadata({
     },
     other: {
       ...(await fetchMetadata(framesUrl)),
-      "fc:frame:cast_action:url": castActionUrl.toString()
+      "fc:frame:cast_action:url": castActionUrl.toString(),
+      "name": "talentapp:project_verification",
+    "content": "b70c8833bbf0f77768198ee82925ee16a05a8b87eb5a34dd3d19d129f7f73c7327123836617fefcd7c04d96933d76707431f8f0651e20ba62a8aee8b9cd64d9a"
     },
   };
 }
